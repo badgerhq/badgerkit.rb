c4cca89107cc50598df538fb5644b18cdf132b1e
Johan van Zonneveld <johan@vzonneveld.nl>
2014-04-23 16:32:09 +0200

    Fix initialize bug

--- lib/badgekit.rb
+++ lib/badgekit.rb
@@ -3,13 +3,19 @@ require "badgekit/client"
 
 module Badgekit
 
-  # Alias for Badgekit::Client.new
-  #
-  # @return [Badgekit::Client.]
-  #
-  def new(*args)
-    @options = options
-    Badgekit::Client.new(*args)
-  end
+  ### Class methods
+
+  class << self
+
+    # Alias for Badgekit::Client.new
+    #
+    # @return [Badgekit::Client.]
+    #
+    def new(*args)
+      @options = options
+      Badgekit::Client.new(*args)
+    end
+
+  end # class << self
 
 end # Badgekit
